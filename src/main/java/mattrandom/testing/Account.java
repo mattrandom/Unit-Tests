fe408799a08dd918c030f59b2bc5f39d69d03aba
mattrandom <44298255+mattrandom@users.noreply.github.com>
2020-06-09 18:32:38 +0200

    2.9 First test

---
+++ src/main/java/mattrandom/testing/Account.java
@@ -0,0 +1,20 @@
+package mattrandom.testing;
+
+public class Account {
+
+    private boolean active;
+
+    public Account() {
+        this.active = false;
+    }
+
+    public void activate() {
+        this.active = true;
+    }
+
+    public boolean isActive() {
+        return this.active;
+    }
+
+
+}
---
+++ src/test/java/mattrandom/testing/AccountTest.java
@@ -0,0 +1,15 @@
+package mattrandom.testing;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+public class AccountTest {
+
+    @Test
+    public void myTest() {
+        Account newAccount = new Account();
+
+        assertFalse(newAccount.isActive());
+    }
+}
