442e3b6a3c77efe6433d0234dcc20a03e5f28dd1
mattrandom <44298255+mattrandom@users.noreply.github.com>
2020-10-07 19:02:30 +0200

    JUnit5 / Disabled & DisplayName

--- src/test/java/testing/CartTest.java
+++ src/test/java/testing/CartTest.java
@@ -1,14 +1,17 @@
 package testing;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTimeout;
 
+@DisplayName("Test cases for Cart")
 class CartTest {
 
     @Test
+    @DisplayName("Cart is able to process 1000 orders in 25 ms")
     void simulateLargeOrder() {
         //given
         Cart cart = new Cart();
