647940b989bb21e2c69dfef4b848ea912e8d9a49
mattrandom <44298255+mattrandom@users.noreply.github.com>
2020-10-07 18:43:06 +0200

    JUnit5 / Testing Timeout

---
+++ src/main/java/testing/Cart.java
@@ -0,0 +1,27 @@
+package testing;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cart {
+    private List<Order> orders = new ArrayList<>();
+
+    public void addOrderToCart(Order order) {
+        this.orders.add(order);
+    }
+
+    public void clearCart() {
+        this.orders.clear();
+    }
+
+    public void simulateLargeOrder() {
+        for (int i = 0; i < 1000; i++) {
+            Meal meal = new Meal(i % 10, "Cheeseburger no " + i);
+            Order order = new Order();
+            order.addMealToOrder(meal);
+            addOrderToCart(order);
+        }
+        System.out.println("Cart size: " + orders.size());
+        clearCart();
+    }
+}
---
+++ src/test/java/testing/CartTest.java
@@ -0,0 +1,19 @@
+package testing;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CartTest {
+
+    @Test
+    void simulateLargeOrder() {
+        //given
+        Cart cart = new Cart();
+
+        //when & then
+        assertTimeout(Duration.ofMillis(25), cart::simulateLargeOrder);
+    }
+}
